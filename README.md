72c39b0df1be73878211ba20e94b0bea6f893902
lj2007331 <lj2007331@gmail.com>
2015-06-08 14:38:03 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-## Alert: The project will stop for updates on November 9th, 2013
-## This project already Combined with LNMP, For more information Please visit https://github.com/lj2007331/lnmp 
+   This script is free collection of shell scripts for rapid deployment of `LAMP` stacks (`Linux`, `Apache`, `MySQL`/`MariaDB`/`Percona` and `PHP`) for CentOS/Redhat Debian and Ubuntu.
+
+   Script features: 
+- Constant updates 
+- Source compiler installation, most source code is the latest stable version, and downloaded from the official website
+- Fixes some security issues 
+- You can freely choose to install database version (MySQL-5.6, MySQL-5.5, MariaDB-10.0, MariaDB-5.5, Percona-5.6, Percona-5.5)
+- You can freely choose to install PHP version (php-5.3, php-5.4, php-5.5, php-5.6, php-7/phpng(alpha))
+- You can freely choose to install Apache version (Apache-2.4, Apache-2.2)
+- According to their needs can to install ZendOPcache, xcache, APCU, eAccelerator, ionCube and ZendGuardLoader (php-5.4, php-5.3)
+- According to their needs can to install Pureftpd, phpMyAdmin
+- According to their needs can to install memcached, redis
+- According to their needs can to optimize MySQL with jemalloc or tcmalloc
+- Add a virtual host script provided
+- Redis, phpMyAdmin upgrade script provided
+- Add backup script provided
+
+## How to use 
+
+```bash
+   yum -y install wget screen # for CentOS/Redhat
+   #apt-get -y install wget screen # for Debian/Ubuntu 
+   wget http://mirrors.linuxeye.com/lamp.tar.gz
+   tar xzf lamp.tar.gz
+   cd lamp
+   # Prevent interrupt the installation process. If the network is down, 
+   # you can execute commands `screen -r lamp` network reconnect the installation window.
+   screen -S lamp
+   ./install.sh
+```
+
+## How to add a virtual host
+
+```bash
+   ./vhost.sh
+```
+
+## How to add FTP virtual user 
+
+```bash
+   ./pureftpd_vhost.sh
+```
+
+## How to backup
+
+```bash
+   ./backup_setup.sh # Set backup options 
+   ./backup.sh # Start backup, You can add cron jobs
+   # crontab -l # Examples 
+     0 1 * * * cd ~/lamp;./backup.sh  > /dev/null 2>&1 &
+```
+
+## How to manage service
+MySQL/MariaDB/Percona:
+```bash
+   service mysqld {start|stop|restart|reload|status}
+```
+PHP:
+```bash
+   service php-fpm {start|stop|restart|reload|status}
+```
+Apache:
+```bash
+   service httpd {start|restart|stop}
+```
+Pure-Ftpd:
+```bash
+   service pureftpd {start|stop|restart|status}
+```
+Redis:
+```bash
+   service redis-server {start|stop|status|restart|reload}
+```
+Memcached:
+```bash
+   service memcached {start|stop|status|restart|reload}
+```
+
+## How to upgrade 
+```bash
+   ./upgrade_php.sh # upgrade PHP
+   ./upgrade_redis.sh # upgrade Redis 
+   ./upgrade_phpmyadmin.sh # upgrade phpMyAdmin 
+```
+
+## How to uninstall 
+
+```bash
+   ./uninstall.sh
+```
+
+## Installation
+   For feedback, questions, and to follow the progress of the project (Chinese): <br />
+   [lamp最新源码一键安装脚本](http://blog.linuxeye.com/82.html)<br />
--- vhost.sh
+++ vhost.sh
@@ -1,94 +1,118 @@
 #!/bin/bash
+# Author:  yeho <lj2007331 AT gmail.com>
+# Blog:  http://blog.linuxeye.com
 
 # Check if user is root
-[ $(id -u) != "0" ] && echo "Error: You must be root to run this script, please use root to create vhost" && exit 1
+[ $(id -u) != "0" ] && { echo -e "\033[31mError: You must be root to run this script\033[0m"; exit 1; } 
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+clear
+printf "
+#######################################################################
+#    LNMP/LAMP/LANMP for CentOS/RadHat 5+ Debian 6+ and Ubuntu 12+    #
+# For more information please visit http://blog.linuxeye.com/31.html  #
+#######################################################################
+"
+. ./options.conf
 
-echo "#######################################################################"
-echo "#                    LNMP for CentOS/RadHat 5/6                       #"
-echo "# For more information please visit http://blog.linuxeye.com/82.html  #"
-echo "#######################################################################"
-echo ''
-
+Input_domain()
+{
 while :
 do
-	read -p "Please input domain(example: www.linuxeye.com linuxeye.com):" domain
+	echo
+	read -p "Please input domain(example: www.linuxeye.com): " domain
 	if [ -z "`echo $domain | grep '.*\..*'`" ]; then
-		echo -e "\033[31minput error\033[0m"
+		echo -e "\033[31minput error! \033[0m"
 	else
 		break
 	fi
 done
-if [ ! -f "/usr/local/apache/conf/vhost/$domain.conf" ]; then
-	echo "################################"
-	echo "domain=$domain"
-	echo "################################"
-else
-	echo "################################"
-	echo "$domain is exist!"
-	echo "################################"
+
+if [ -e "$web_install_dir/conf/vhost/$domain.conf" -o -e "$apache_install_dir/conf/vhost/$domain.conf" ]; then
+	[ -e "$apache_install_dir/conf/vhost/$domain.conf" ] && echo -e "$domain in the Apache already exist! \nYou can delete \033[32m$apache_install_dir/conf/vhost/$domain.conf\033[0m and re-create"
 	exit 1
-fi
-
-read -p "Do you want to add more domain name? (y/n)" add_more_domainame
-if [ "$add_more_domainame" == 'y' ]; then
-	while :
-	do
-		read -p "Type domainname,example(blog.linuxeye.com bbs.linuxeye.com):" moredomain
-		if [ -z "`echo $moredomain | grep '.*\..*'`" ]; then
-			echo -e "\033[31minput error\033[0m"
-		else
-			echo "################################"
-			echo domain list="$moredomain"
-			echo "################################"
-			moredomainame=" $moredomain"
-			break
-		fi
-	done
-	sa=ServerAlias$moredomainame
-fi
-
-echo "Please input the directory for the domain:$domain :"
-read -p "(Default directory: /home/wwwroot/$domain):" vhostdir
-if [ -z "$vhostdir" ]; then
-	vhostdir="/home/wwwroot/$domain"
-fi
-echo "################################"
-echo Virtual Host Directory="$vhostdir"
-echo "################################"
-
-echo "################################"
-read -p "Allow access_log? (y/n)" access_log
-echo "################################"
-
-if [ "$access_log" == 'n' ]; then
-	al='CustomLog "/dev/null" common'
 else
-	al=CustomLog \"/home/wwwlogs/${domain}-access.log common\"
-	echo "################################"
-	echo You access log file="/home/wwwlogs/${domain}-access.log"
-	echo "################################"
+	echo "domain=$domain"
 fi
 
+while :
+do
+	echo ''
+        read -p "Do you want to add more domain name? [y/n]: " moredomainame_yn 
+        if [ "$moredomainame_yn" != 'y' ] && [ "$moredomainame_yn" != 'n' ];then
+                echo -e "\033[31minput error! Please only input 'y' or 'n'\033[0m"
+        else
+                break 
+        fi
+done
 
-[ ! -d /usr/local/apache/conf/vhost ] && mkdir /usr/local/apache/conf/vhost
+if [ "$moredomainame_yn" == 'y' ]; then
+        while :
+        do
+                echo
+                read -p "Type domainname,example(linuxeye.com www.example.com): " moredomain
+                if [ -z "`echo $moredomain | grep '.*\..*'`" ]; then
+                        echo -e "\033[31minput error\033[0m"
+                else
+			[ "$moredomain" == "$domain" ] && echo -e "\033[31mDomain name already exists! \033[0m" && continue
+                        echo domain list="$moredomain"
+                        moredomainame=" $moredomain"
+                        break
+                fi
+        done
+        Domain_alias=ServerAlias$moredomainame
+fi
 
+echo
+echo "Please input the directory for the domain:$domain :"
+read -p "(Default directory: /home/wwwroot/$domain): " vhostdir
+if [ -z "$vhostdir" ]; then
+        vhostdir="/home/wwwroot/$domain"
+        echo -e "Virtual Host Directory=\033[32m$vhostdir\033[0m"
+fi
+echo
 echo "Create Virtul Host directory......"
 mkdir -p $vhostdir
 echo "set permissions of Virtual Host directory......"
-chown -R www.www $vhostdir
+chown -R ${run_user}.$run_user $vhostdir
+}
 
-cat >/usr/local/apache/conf/vhost/$domain.conf<<EOF
+Apache_log()
+{
+while :
+do
+        echo ''
+        read -p "Allow Apache access_log? [y/n]: " access_yn
+        if [ "$access_yn" != 'y' ] && [ "$access_yn" != 'n' ];then
+                echo -e "\033[31minput error! Please only input 'y' or 'n'\033[0m"
+        else
+                break
+        fi
+done
+
+if [ "$access_yn" == 'n' ]; then
+        A_log='CustomLog "/dev/null" common'
+else
+        A_log="CustomLog \"/home/wwwlogs/${domain}_apache.log\" common"
+        echo "You access log file=/home/wwwlogs/${domain}_apache.log"
+fi
+}
+
+Create_apache_conf()
+{
+[ "`$apache_install_dir/bin/apachectl -v | awk -F'.' /version/'{print $2}'`" == '4' ] && R_TMP='Require all granted' || R_TMP=
+[ ! -d $apache_install_dir/conf/vhost ] && mkdir $apache_install_dir/conf/vhost
+cat > $apache_install_dir/conf/vhost/$domain.conf << EOF
 <VirtualHost *:80>
-    ServerAdmin webmaster@$domain
+    ServerAdmin admin@linuxeye.com 
     DocumentRoot "$vhostdir"
-    ServerName $domain 
-    $sa
-    ErrorLog "/home/wwwlogs/${domain}-error.log"
-    $al
+    ServerName $domain
+    $Domain_alias
+    ErrorLog "/home/wwwlogs/${domain}_error_apache.log"
+    $A_log
 <Directory "$vhostdir">
     SetOutputFilter DEFLATE
     Options FollowSymLinks
-    Require all granted
+    $R_TMP
     AllowOverride All
     Order allow,deny
     Allow from all
@@ -97,18 +121,30 @@ cat >/usr/local/apache/conf/vhost/$domain.conf<<EOF
 </VirtualHost>
 EOF
 
-echo "Test Apache configure file......"
-/usr/local/apache/bin/apachectl -t
-echo ""
-echo "Restart Apache......"
-/sbin/service httpd restart
+echo
+$apache_install_dir/bin/apachectl -t
+if [ $? == 0 ];then
+	echo "Restart Apache......"
+	/etc/init.d/httpd restart
+else
+	rm -rf $apache_install_dir/conf/vhost/$domain.conf
+	echo -e "Create virtualhost ... \033[31m[FAILED]\033[0m"
+	exit 1
+fi
 
-echo "#######################################################################"
-echo "#                    LNMP for CentOS/RadHat 5/6                       #"
-echo "# For more information please visit http://blog.linuxeye.com/82.html  #"
-echo "#######################################################################"
-echo ''
-echo -e "`printf "%-40s" "Your domain:"`\033[32m$domain\033[0m"
-echo -e "`printf "%-40s" "Directory of $domain:"`\033[32m$vhostdir\033[0m"
-echo ''
-echo "#######################################################################"
+printf "
+#######################################################################
+#         LAMP for CentOS/RadHat 5+ Debian 6+ and Ubuntu 12+          #
+# For more information please visit http://blog.linuxeye.com/82.html  #
+#######################################################################
+"
+echo -e "`printf "%-32s" "Your domain:"`\033[32m$domain\033[0m"
+echo -e "`printf "%-32s" "Virtualhost conf:"`\033[32m$apache_install_dir/conf/vhost/$domain.conf\033[0m"
+echo -e "`printf "%-32s" "Directory of $domain:"`\033[32m$vhostdir\033[0m"
+}
+
+if [ -d "$web_install_dir" -a -d "$apache_install_dir" -a "$web_install_dir" == "$apache_install_dir" ];then
+	Input_domain
+	Apache_log
+	Create_apache_conf
+fi 
